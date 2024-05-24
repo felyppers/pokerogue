b86e7518b2f640cb281bcc4ff49ece0011ebafcc
Felyppe Rodrigues <felyppers@gmail.com>
2024-05-24 16:06:48 +0200

    Added call to pokerogue.net

--- src/account.ts
+++ src/account.ts
@@ -12,7 +12,8 @@ export const clientSessionId = Utils.randomString(32);
 export function updateUserInfo(): Promise<[boolean, integer]> {
   return new Promise<[boolean, integer]>(resolve => {
     if (bypassLogin) {
-      loggedInUser = { username: 'Guest', lastSessionSlot: -1 };
+      const localStorageUsername = localStorage.getItem(`username`);
+      loggedInUser = { username: localStorageUsername === undefined ? 'Guest' : localStorageUsername, lastSessionSlot: -1 };
       let lastSessionSlot = -1;
       for (let s = 0; s < 5; s++) {
         if (localStorage.getItem(`sessionData${s ? s : ''}_${loggedInUser.username}`)) {
